b69e8b6aa287b4764b577253ceeb634d93e4c47f
Kevin Chen <me@kevchentw.com>
2017-11-20 00:30:21 +0800

    add WritableNativeMap

--- android/src/main/java/com/zxcpoiu/incallmanager/InCallManagerModule.java
+++ android/src/main/java/com/zxcpoiu/incallmanager/InCallManagerModule.java
@@ -41,6 +41,7 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.UiThreadUtil;
 import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.WritableNativeMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 import java.lang.reflect.Field;
